52878f7c9e51e0e07932b18dda981666a9d8c783
SAYAK DHAR <sayakdhar1999@gmail.com>
2020-11-25 16:55:20 +0530

    Add files via upload

---
+++ Assets/Scenes/Ball.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    float speed;
+
+    float radius;
+    Vector2 direction;
+
+    void Start()
+    {
+        direction = Vector2.one.normalized;
+        radius = transform.localScale.x / 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate (direction * speed * Time.deltaTime);
+
+        if (transform.position.y < GM.bottomLeft.y + radius && direction.y < 0) {
+        	direction.y = -direction.y;
+        }
+        if (transform.position.y > GM.topRight.y - radius && direction.y > 0) {
+        	direction.y = -direction.y;
+        }
+
+        if (transform.position.x < GM.bottomLeft.x + radius && direction.x < 0) {
+        	Debug.Log ("Right player Wins!!");
+        	Time.timeScale = 0;
+        	enabled = false;
+        }
+        if (transform.position.x > GM.topRight.x - radius && direction.x > 0) {
+        	Debug.Log ("Left player Wins!!");
+        	Time.timeScale = 0;
+        	enabled = false;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+    	if(other.tag == "Paddle") {
+    		bool isRight = other.GetComponent<Paddle> ().isRight;
+
+    		if (isRight == true && direction.x > 0) {
+    			direction.x = -direction.x;
+    		}
+    		if (isRight == false && direction.x < 0) {
+    			direction.x = -direction.x;
+    		}
+    	}
+    }
+}
---
+++ Assets/Scenes/Paddle.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    float speed;
+
+    float height;
+
+    string input;
+    public bool isRight;
+
+    void Start()
+    {
+        height = transform.localScale.y;
+        
+    }
+
+    public void Init(bool isRightPaddle) {
+
+    	isRight = isRightPaddle;
+
+    	Vector2 pos = Vector2.zero;
+
+    	if (isRightPaddle){
+    		pos = new Vector2(GM.topRight.x, 0);
+    		pos -= Vector2.right * transform.localScale.x;
+
+    		input = "PaddleRight";
+
+    		}else {
+    			pos = new Vector2(GM.bottomLeft.x, 0);
+    			pos += Vector2.right * transform.localScale.x;
+
+    			input = "PaddleLeft";
+
+    		}
+    		transform.position = pos;
+    		transform.name = input;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float move = Input.GetAxis(input) * Time.deltaTime * speed;
+
+        if (transform.position.y < GM.bottomLeft.y + height / 2 && move < 0){
+        	move = 0;
+        }
+        if (transform.position.y > GM.topRight.y - height / 2 && move > 0){
+        	move = 0;
+        }
+
+        transform.Translate (move * Vector2.up);
+    }
+}
